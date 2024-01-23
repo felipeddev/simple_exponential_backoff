aa27e51f55ea0152f549666935970706c58d40e0
Felipe Duarte Silva <feliped.dev.contato@gmail.com>
2024-01-23 09:58:28 -0300

    Initial commit

---
+++ example/exponential_backoff_example.dart
@@ -0,0 +1,6 @@
+import 'package:exponential_backoff/exponential_backoff.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/exponential_backoff_test.dart
@@ -0,0 +1,16 @@
+import 'package:exponential_backoff/exponential_backoff.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
